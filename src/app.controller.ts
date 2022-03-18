b8ffec63232d81dd78018565ef2d6c994cf2fef1
tanvirAlamOmi <tanviralamomi@gmail.com>
2022-03-18 22:52:11 +0600

    mongoose and env setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService, 
+    private configService: ConfigService
+  ) {}
 
   @Get()
   getHello(): string {
+    console.log(this.configService.get<string>('Q'));
+    
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServicesModule } from './services/services.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // Third party module
+    ConfigModule.forRoot({cache:true}),
+    MongooseModule.forRoot('mongodb+srv://lazarus:2441139@dhar-chai.fg0kz.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-83s00b-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'),
+    
+    // App module
+    ServicesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
